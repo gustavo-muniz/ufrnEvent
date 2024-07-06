700bafaa13ecca3f3e687e73a60671b2682f158b
Gustavo Muniz <gustamunizbr@gmail.com>
2024-07-06 15:26:20 -0300

    adds Category, Activity

---
+++ src/main/java/com/munizdev/ufrnEvent/entities/Category.java
@@ -0,0 +1,62 @@
+package com.munizdev.ufrnEvent.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String description;
+
+    @OneToMany(mappedBy = "category")
+    private List<Activity> activities = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public Category(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
