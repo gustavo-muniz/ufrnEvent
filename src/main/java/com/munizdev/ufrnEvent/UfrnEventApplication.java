acec0c1c654ea25afe6cffbd22a7fb1a927f7366
Gustavo Muniz <gustamunizbr@gmail.com>
2024-07-06 13:38:32 -0300

    project created

---
+++ src/main/java/com/munizdev/ufrnEvent/UfrnEventApplication.java
@@ -0,0 +1,13 @@
+package com.munizdev.ufrnEvent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfrnEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfrnEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/munizdev/ufrnEvent/UfrnEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munizdev.ufrnEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfrnEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
